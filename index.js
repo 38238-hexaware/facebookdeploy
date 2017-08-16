d6822b7c947e3d4fe9b424df923a7578329f7fe9
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-16 17:16:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.post("/", function (req, res) {
     }
 });
 
-
+app.get("/getdata/",function(req, res){
+   fs.readFile("./data.json",'utf-8',function(err,data){
+      res.json(data);
+   });
+   
+});
 app.listen(process.env.PORT || 3000, function (message) {
     console.log("Server is running on the port...");
 })
