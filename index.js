ee23e948a705aa2f473bc7b959475800683e2ab4
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-16 17:15:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
 var express = require('express');
 var app = express();
-var fs=require('fs');
 var bodyParser=require('body-parser');
-var jsonbody='';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/getdata/",function(req, res){
-   fs.readFile("./data.json",function(err,data){
-      res.json(data);
-   });
-   
-});
 app.post("/", function (req, res) {
-   console.log(req.body.object);
-   jsonbody=req.body.object;
-   fs.writeFiles("./data.json",req.body.object,"utf-8",function(err){
-      if(err){
-         console.log('error');
-      }
-   });
     if (req.body.result.action == "input.welcome") {
         if (req.body.result.resolvedQuery == "hi") {
             return res.json({
-                speech: req.body,
-                displayText: req.body,
+                speech: "How are you?? Srini",
+                displayText: "How are you?? Srini",
                 source: 'agent'
             });
         }
-    });
-    
+    }
+});
 
 
 app.listen(process.env.PORT || 3000, function (message) {
