b5a960216c6295b6799d41af22c6663613331101
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-16 11:24:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,16 @@ var rec_id=req.body.originalRequest.data.recipient.id;
 
     if (req.body.result.action == "input.welcome") {
         if (req.body.result.resolvedQuery == "hi") {
-         let bodys=JSON.parse(fs.readFileSync(fburl+sender_id+"?access_token="+FACEBOOK_ACCESS_TOKEN, "utf8"));
-         
+         request({
+            uri: fburl+sender_id+"?access_token="+FACEBOOK_ACCESS_TOKEN,
+            methos: 'GET'
+        }, (err, response, body) => {
           return res.json({
                 speech:"Welcome, "+bodys.first_name+" "+bodys.last_name,
                 displayText: "Welcome, "+bodys.first_name+" "+bodys.last_name,
                 source: 'agent'
             });
-       
+        });
        
               
             
