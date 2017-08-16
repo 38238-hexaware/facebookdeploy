c6e4566319238c249e6687046396b9b4831774a3
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-16 09:05:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs=require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/", function (req, res) {
-fs.writeFiles("./data.json",req.body,'utf-8',function(err){
+fs.writeFile("./data.json",req,'utf-8',function(err){
 });
 if (req.body.object === 'page') {
     if (req.body.result.action == "input.welcome") {
